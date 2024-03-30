703367c0294bfd8461da877a069cbf326ba13a48
Ted Ralphs <ted@lehigh.edu>
2024-03-30 14:03:31 -0400

    cat the config.log in the subdirectory

--- recipe/build.sh
+++ recipe/build.sh
@@ -39,7 +39,7 @@ fi
     --exec-prefix="${USE_PREFIX}" \
     --with-blas="${WITH_BLAS_LIB}" \
     --with-lapack="${WITH_LAPACK_LIB}" \
-    ${WIN_FLAGS} || cat config.log
+    ${WIN_FLAGS} || cat CoinUtils\config.log
 
 make -j "${CPU_COUNT}"
 make install
