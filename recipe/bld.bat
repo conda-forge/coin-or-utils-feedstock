14a82f9ec3ffea0ce368057e67c169bcd8093908
Julianus Pfeuffer <julianus.pfeuffer@pfizer.com>
2024-03-31 21:35:49 +0200

    try some stuff for preserving paths in msys2 (from dotnet recipe)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,1 +1,10 @@
-bash "%RECIPE_DIR%\build.sh"
+copy "%RECIPE_DIR%\build.sh" .
+set PREFIX=%PREFIX:\=/%
+set SRC_DIR=%SRC_DIR:\=/%
+set MSYSTEM=MINGW%ARCH%
+set MSYS2_PATH_TYPE=inherit
+set CHERE_INVOKING=1
+set BUILD_PLATFORM=win-64
+
+
+bash -lc "./build.sh"
