8f14e6c127e52f22588f320d18355c8e80df74f0
Julianus Pfeuffer <julianus.pfeuffer@pfizer.com>
2024-04-01 21:37:48 +0200

    add back updated config.sub/guess

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+
+# Get an updated config.sub and config.guess 
+cp $BUILD_PREFIX/share/gnuconfig/config.* ./CoinUtils 
+cp $BUILD_PREFIX/share/gnuconfig/config.* .
+
 set -e
 
 if [[ "${target_platform}" == win-* ]]; then
