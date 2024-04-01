5cff18bc2f388e9414503a437d708eb612c14e79
Julianus Pfeuffer <julianus.pfeuffer@pfizer.com>
2024-04-01 15:44:58 +0200

    more cleanup

--- recipe/build.sh
+++ recipe/build.sh
@@ -21,12 +21,8 @@ export OPENBLAS_NUM_THREADS=1
 
 if [ ! -z ${LIBRARY_PREFIX+x} ]; then
     USE_PREFIX=$LIBRARY_PREFIX
-    WITH_BLAS_LIB="${LIBRARY_PREFIX}\lib\cblas.lib"
-    WITH_LAPACK_LIB="${LIBRARY_PREFIX}\lib\lapack.lib"
 else
     USE_PREFIX=$PREFIX
-    WITH_BLAS_LIB="-L${PREFIX}/lib -lblas"
-    WITH_LAPACK_LIB="-L${PREFIX}/lib -llapack"
     export CFLAGS="${CFLAGS} -O3"
     export CXXFLAGS="${CXXFLAGS} -O3"
     export CXXFLAGS="${CXXFLAGS} -std=c++11"
@@ -34,7 +30,6 @@ fi
 
 
 if [[ "${target_platform}" == win-* ]]; then
-    #WIN_FLAGS="--with-blas-incdir=-I${LIBRARY_PREFIX}\include\cblas --build=x86_64-w64-mingw32 --enable-msvc"
     WIN_FLAGS="--build=x86_64-w64-mingw32 --enable-msvc"
 fi
 
