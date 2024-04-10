520a6d5166be4dbb9b4d9bec3f8369c9506ef4a1
Julianus Pfeuffer <jule.pf@gmail.com>
2024-04-10 10:00:18 +0000

    remove all mentions of fortran, blas, lapack; address review; try macos c17

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,18 +8,7 @@ else
   # Get an updated config.sub and config.guess (for mac arm and lnx aarch64)
   cp $BUILD_PREFIX/share/gnuconfig/config.* ./CoinUtils 
   cp $BUILD_PREFIX/share/gnuconfig/config.* .
-  export CXXFLAGS="${CXXFLAGS} -std=c++11" # macOS clang defaults to cxx17 but coinutils 2.11.10 still has register kws
 fi
-
-if [[ "${target_platform}" == linux-* ]]; then
-    export FLIBS="-lgcc_s -lgcc -lstdc++ -lm"
-fi
-
-# Use only 1 thread with OpenBLAS to avoid timeouts on CIs.
-# This should have no other affect on the build. A user
-# should still be able to set this (or not) to a different
-# value at run-time to get the expected amount of parallelism.
-export OPENBLAS_NUM_THREADS=1
 
 if [ ! -z ${LIBRARY_PREFIX+x} ]; then
     USE_PREFIX=$LIBRARY_PREFIX
