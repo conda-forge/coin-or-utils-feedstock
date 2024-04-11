be37221f16a18eb095b01ed143201870ffc15517
Ted Ralphs <tkralphs@gmail.com>
2024-04-11 10:10:38 -0400

    Link Lapack on Windows

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,29 +9,23 @@ else
 fi
 
 if [[ "${target_platform}" == win-* ]]; then
-  ls -la ${LIBRARY_PREFIX}/lib/
   EXTRA_FLAGS="--enable-msvc"
   BLAS_LIB="${LIBRARY_PREFIX}/lib/cblas.lib"
-
-  ./configure \
-    --prefix="${USE_PREFIX}" \
-    --exec-prefix="${USE_PREFIX}" \
-    --with-blas-lib="${BLAS_LIB}" \
-    ${EXTRA_FLAGS} || cat CoinUtils/config.log
+  LAPACK_LIB="${LIBRARY_PREFIX}/lib/lapack.lib"
 else
   # Get an updated config.sub and config.guess (for mac arm and lnx aarch64)
   cp $BUILD_PREFIX/share/gnuconfig/config.* ./CoinUtils 
   cp $BUILD_PREFIX/share/gnuconfig/config.* .
   BLAS_LIB="-L${PREFIX}/lib -lblas"
   LAPACK_LIB="-L${PREFIX}/lib -llapack"
-
-  ./configure \
-    --prefix="${USE_PREFIX}" \
-    --exec-prefix="${USE_PREFIX}" \
-    --with-blas-lib="${BLAS_LIB}" \
-    --with-lapack-lib="${LAPACK_LIB}" \
-    ${EXTRA_FLAGS} || cat CoinUtils/config.log
 fi
+
+./configure \
+  --prefix="${USE_PREFIX}" \
+  --exec-prefix="${USE_PREFIX}" \
+  --with-blas-lib="${BLAS_LIB}" \
+  --with-lapack-lib="${LAPACK_LIB}" \
+  ${EXTRA_FLAGS} || cat CoinUtils/config.log
 
 make -j "${CPU_COUNT}"
 
